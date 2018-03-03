23b69f1973597c15df50e04859f0075925edbf15
andrecarrsd2008 <andrecarrsd2008@gmail.com>
2018-03-02 22:18:03 -0800

    I have one of two windows deployed

---
+++ assets/javascript/app.js
@@ -0,0 +1,56 @@
+var library
+var count=30;
+
+var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+function timer()
+{
+  count=count-1;
+  if (count <= 0)
+  {
+     clearInterval(counter);
+     //counter ended, do something here
+     return;
+  }
+
+
+{ "Question 1"
+    What was the first full length GCI movie?
+    Answers 'A Bugs Life', 'Monsters Inc.', 'Toy Story', 'The Lion King'
+}
+
+{"Question 2"
+   Which of these is not the name of one of the Spice Girls?
+   Answers 'Sporty Spice', 'Fred Spice' 'Scary Spice', 'Posh Spice'
+}
+
+{ "Question 3"
+   Which NBA team won the most titles in the 90s?
+   Answers 'New York Knicks', 'Portland Trailblazers', 'Los Angeles Lakers', 'Chicago Bulls'
+}
+
+{"Question 4"
+ Which group released the hit song, "Smells Like Teen Spirit?"
+ Answers 'Nirvana', 'Backstreet Boys', 'The Offspring', 'No Doubt'
+}
+
+{"Question 5"
+Which popular Disney movie featured the song "Circle of Life?"
+Answers 'Aladdin', 'Hercules', 'Mulan', 'The Lion King'
+}
+
+{"Question 6"
+Finish this line from the Fresh Prince of Bel-Air theme song: 'I whisteled for a cab
+and when it came near, the license plate said...'
+Answers 'Dice', 'Mirror' 'Fresh' 'Cab'
+}
+
+{"Question 7"
+What was Doug's best friend's name?
+Answers 'Skeeter', 'Mark', 'Zach', 'Cody'
+}
+
+{"Question 8"
+What was the name of the principal at Bayside High in Saved By the Bell?
+Answer 'Mr. Zhou', 'Mr. Driggers', 'Mr. Belding' 'Mr. Page'
+}
